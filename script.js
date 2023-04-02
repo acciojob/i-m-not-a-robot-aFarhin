58bc4117bb1f42c627a33a9cbdc64938cd61c635
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-02 20:09:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,91 @@
 //your JS code here. If required.
-const imageUrls = [/* 6 image URLs */];
-const duplicatedClass = `img${Math.floor(Math.random() * 5) + 1}`;
-
-const images = document.querySelectorAll('img');
-const verifyBtn = document.getElementById('verify');
-const resetBtn = document.getElementById('reset');
-const para = document.getElementById('para');
-
-let selectedImages = [];
-
-// Fisher-Yates shuffle algorithm
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
+let image = [];
+for (let i = 0; i < 5; i++) {
+  let t = document.createElement("IMG");
+  t.setAttribute("data-ns-test", `img${i + 1}`);
+  t.width = 100;
+  t.height = 100;
+  t.onclick = (e) => captchaClick(e);
+  t.src = `images/${i + 1}.jpg`;
+  image.push(t);
+}
+let temp = Math.floor(Math.random() * 5);
+let t = document.createElement("IMG");
+t.setAttribute("data-ns-test", `img${temp + 1}`);
+t.width = 100;
+t.height = 100;
+t.onclick = (e) => captchaClick(e);
+t.src = `images/${temp + 1}.jpg`;
+image.push(t);
+image.sort(() => Math.random() - 0.5);
+for (let i = 0; i < 6; i++) {
+  document.getElementById("main").appendChild(image[i]);
 }
 
-function resetState() {
-  selectedImages = [];
-  images.forEach(image => image.classList.remove('selected'));
-  verifyBtn.style.display = 'none';
-  para.innerText = '';
+let captcha = [];
+function clearCaptcha() {
+  // console.log("Clearing captcha");
+  for (let i = 0; i < 6; i++) {
+    image[i].onclick = (e) => captchaClick(e);
+  }
+  captcha = [];
+  try {
+    document.getElementById("para").remove();
+  } catch (e) {}
+  try {
+    document.getElementById("btn").remove();
+  } catch (e) {}
+  try {
+    document.getElementById("reset").remove();
+  } catch (e) {}
 }
 
-function handleImageClick(e) {
-  const clickedImage = e.target;
-  if (clickedImage.classList.contains('selected')) {
-    return;
+function captchaClick(e) {
+  console.log(e.target.attributes["data-ns-test"].nodeValue);
+  captcha.push(e.target.attributes["data-ns-test"].nodeValue);
+  e.target.onclick = () => {};
+  // console.log(captcha);
+
+  if (captcha.length === 1) {
+    let p = document.createElement("button");
+    p.id = "reset";
+    p.innerHTML = "Reset";
+    p.onclick = () => {
+      clearCaptcha();
+    };
+    document.getElementById("main").appendChild(p);
   }
 
-  clickedImage.classList.add('selected');
-  selectedImages.push(clickedImage);
+  if (captcha.length === 2) {
+    let t = document.createElement("button");
+    t.id = "btn";
+    t.innerHTML = "Verify";
+    t.onclick = () => {
+      captchaVerify();
+    };
+    document.getElementById("main").appendChild(t);
+  } else if (captcha.length > 2) {
+    try {
+      document.getElementById("btn").remove();
+    } catch (e) {}
+  }
+  try {
+    document.getElementById("para").remove();
+  } catch (e) {}
+}
 
-  if (selectedImages.length === 2) {
-    verifyBtn.style.display = '
-
+function captchaVerify() {
+  if (captcha.length === 2 && captcha[0] === captcha[1]) {
+    let t = document.createElement("P");
+    t.innerHTML = "You are a human. Congratulations!";
+    t.id = "para";
+    document.getElementById("main").appendChild(t);
+  } else {
+    let t = document.createElement("P");
+    t.innerHTML =
+      "We can't verify you as a human. You selected the non-identical tiles.";
+    t.id = "para";
+    document.getElementById("main").appendChild(t);
+  }
+  document.getElementById("btn").remove();
+}
